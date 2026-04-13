6d553b2ba057ff9d425234e62dd1c719d1314368
Tomas1562 <71684235+Tomas1562@users.noreply.github.com>
2026-04-12 21:39:45 -0300

    Crear una Persona con el DNI  y validar

--- src/ejercicio1/MainEjercicio1_a.java
+++ src/ejercicio1/MainEjercicio1_a.java
@@ -4,7 +4,8 @@ public class MainEjercicio1_a {
 
 	public static void main(String[] args) {
 		
-
+		 Persona p = new Persona("AA202020", "Juan", "Perez", null, "", "", "", "");
+	     Persona.verificarDNI(p.getDni());
 	}
 
 }
