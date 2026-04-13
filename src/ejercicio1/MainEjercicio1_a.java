07960f9618deee47ca9b8de9bb7d7090d34da514
Axel <axelpereyra_la12@hotmail.com>
2026-04-13 20:09:04 -0300

    try-carch

--- src/ejercicio1/MainEjercicio1_a.java
+++ src/ejercicio1/MainEjercicio1_a.java
@@ -5,8 +5,17 @@ public class MainEjercicio1_a {
     public static void main(String[] args) {
 
         try {
-            Persona p = new Persona("20202020", "Juan", "Perez", null, "", "", "", "");
+            Persona p = new Persona("AA202020", "Juan", "Perez", null, "", "", "", "");
             Persona.verificarDNI(p.getDni());
+            System.out.println("Persona agregada correctamente");
+
+        } catch (ExVerificarDNI e) {
+        	//Logica DNI invalido
+        }
+
+        try {
+            Persona p2 = new Persona("20202020", "Alexis", "Fernandez", null, "", "", "", "");
+            Persona.verificarDNI(p2.getDni());
             System.out.println("Persona agregada correctamente");
 
         } catch (ExVerificarDNI e) {
