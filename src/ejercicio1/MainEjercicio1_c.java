33c3c0c9625507fccae6c64c04c825af34755b21
Axel <axelpereyra_la12@hotmail.com>
2026-04-15 20:32:28 -0300

    5 empleados,main c

--- src/ejercicio1/MainEjercicio1_c.java
+++ src/ejercicio1/MainEjercicio1_c.java
@@ -1,11 +1,14 @@
 package ejercicio1;
 
-public class MainEjercicio1_c 
-{
+public class MainEjercicio1_c {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
+
+        Empleado e1 = new Empleado("20111111", "Juan", "Perez", null, "", "", "", "", "Admin");
+        Empleado e2 = new Empleado("20222222", "Ana", "Gomez", null, "", "", "", "", "Ventas");
+        Empleado e3 = new Empleado("20333333", "Luis", "Martinez", null, "", "", "", "", "IT");
+        Empleado e4 = new Empleado("20444444", "Sofia", "Lopez", null, "", "", "", "", "RRHH");
+        Empleado e5 = new Empleado("20555555", "Carlos", "Diaz", null, "", "", "", "", "Soporte");
 
     }
-
 }
