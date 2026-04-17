29501a13cc7a17e1db9b0f3383b96aa2a5891247
SebastianCarracedo <sebascarracedo@gmail.com>
2026-04-17 18:30:32 -0300

    Agregué los atributos y un constructor

--- src/ejercicio2/Polideportivo.java
+++ src/ejercicio2/Polideportivo.java
@@ -1,14 +1,29 @@
 package ejercicio2;
 
 public class Polideportivo implements interfazEdificio, InterfazInstalacionDeportiva {
-	
-	
-	@Override
+
+    // Atributos
+    private String nombre;
+    private double superficie;
+    private int tipoInstalacion;
+
+    // Constructor
+    public Polideportivo(String nombre, double superficie, int tipoInstalacion) {
+        this.nombre = nombre;
+        this.superficie = superficie;
+        this.tipoInstalacion = tipoInstalacion;
+    }
+
+    // interfaces
+    @Override
     public int getTipoDeInstalacion() {
+        return tipoInstalacion;
     }
 
     @Override
     public double getSuperficieEdificio() {
+        return superficie;
+        
+        
+        
     }
-
-}
