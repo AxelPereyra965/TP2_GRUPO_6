307fab3c486163bac686797368f7727b8274442b
Axel <axelpereyra_la12@hotmail.com>
2026-04-17 20:41:24 -0300

    dos edificios de oficina

--- src/ejercicio2/MainEjercicio2.java
+++ src/ejercicio2/MainEjercicio2.java
@@ -1,22 +1,30 @@
 package ejercicio2;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class MainEjercicio2 
-{
+public class MainEjercicio2 {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
 
-        ArrayList<Polideportivo> listaPolideportivos = new ArrayList<Polideportivo>();
+        ArrayList<Object> lista = new ArrayList<>();
 
+        // Polideportivos
         Polideportivo p1 = new Polideportivo("Polideportivo Escobar", 1500.0, 1);
         Polideportivo p2 = new Polideportivo("Polideportivo Tigre", 1800.5, 2);
         Polideportivo p3 = new Polideportivo("Polideportivo Pacheco", 2100.75, 3);
 
-        listaPolideportivos.add(p1);
-        listaPolideportivos.add(p2);
-        listaPolideportivos.add(p3);
-    }
+        // Edificios de oficinas
+        EdificioDeOficinas e1 = new EdificioDeOficinas(10);
+        EdificioDeOficinas e2 = new EdificioDeOficinas(20);
 
+        lista.add(p1);
+        lista.add(p2);
+        lista.add(p3);
+        lista.add(e1);
+        lista.add(e2);
+
+
+        }
+    }
 }
