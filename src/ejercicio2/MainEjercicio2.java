c05667acc3ac0835153bf1b5b9c3d19bc90ff72c
Bernardo Fiaschi <162641863+BernardoFiaschi@users.noreply.github.com>
2026-04-17 20:22:02 -0300

    Add files via upload

--- src/ejercicio2/MainEjercicio2.java
+++ src/ejercicio2/MainEjercicio2.java
@@ -1,5 +1,22 @@
 package ejercicio2;
 
-public class MainEjercicio2 {
+import java.util.ArrayList;
+
+public class MainEjercicio2 
+{
+
+    public static void main(String[] args) 
+    {
+
+        ArrayList<Polideportivo> listaPolideportivos = new ArrayList<Polideportivo>();
+
+        Polideportivo p1 = new Polideportivo("Polideportivo Escobar", 1500.0, 1);
+        Polideportivo p2 = new Polideportivo("Polideportivo Tigre", 1800.5, 2);
+        Polideportivo p3 = new Polideportivo("Polideportivo Pacheco", 2100.75, 3);
+
+        listaPolideportivos.add(p1);
+        listaPolideportivos.add(p2);
+        listaPolideportivos.add(p3);
+    }
 
 }
