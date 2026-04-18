b58b055b01fdb92a2e21b7282b6c4b782d608bd3
SebastianCarracedo <sebascarracedo@gmail.com>
2026-04-17 22:45:16 -0300

    Agregué interfazEdificio y atributo superficie

--- src/ejercicio2/EdificioDeOficinas.java
+++ src/ejercicio2/EdificioDeOficinas.java
@@ -1,27 +1,39 @@
 package ejercicio2;
 
-public class EdificioDeOficinas {
+public class EdificioDeOficinas implements interfazEdificio {
 
+    private double superficie;
     private int numeroOficinas;
 
-    public EdificioDeOficinas(int numeroOficinas) 
-    {
-        this.numeroOficinas = numeroOficinas;
-    }
-
-    public int getNumeroOficinas() 
-    {
-        return numeroOficinas;
-    }
-
-    public void setNumeroOficinas(int numeroOficinas) 
-    {
+    public EdificioDeOficinas(double superficie, int numeroOficinas) {
+        this.superficie = superficie;
         this.numeroOficinas = numeroOficinas;
     }
 
     @Override
-    public String toString() 
-    {
-        return "EdificioDeOficinas numero de oficinas=" + numeroOficinas;
+    public double getSuperficieEdificio() {
+        return superficie;
+    }
+
+    public int getNumeroOficinas() {
+        return numeroOficinas;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
+    public void setNumeroOficinas(int numeroOficinas) {
+        this.numeroOficinas = numeroOficinas;
+    }
+
+    @Override
+    public String toString() {
+        return "EdificioDeOficinas superficie=" + superficie + ", numeroOficinas=" + numeroOficinas;
     }
 }
+
--- src/ejercicio2/MainEjercicio2.java
+++ src/ejercicio2/MainEjercicio2.java
@@ -14,26 +14,33 @@ public class MainEjercicio2 {
         Polideportivo p2 = new Polideportivo("Polideportivo Tigre", 1800.5, 2);
         Polideportivo p3 = new Polideportivo("Polideportivo Pacheco", 2100.75, 3);
 
-        // Edificios de oficinas
-        EdificioDeOficinas e1 = new EdificioDeOficinas(10);
-        EdificioDeOficinas e2 = new EdificioDeOficinas(20);
+        // Edificios de oficinas con superficie
+        EdificioDeOficinas e1 = new EdificioDeOficinas(2500.0, 10);
+        EdificioDeOficinas e2 = new EdificioDeOficinas(4000.0, 20);
 
         lista.add(p1);
         lista.add(p2);
         lista.add(p3);
         lista.add(e1);
         lista.add(e2);
-        
-        Iterator<Object> it = lista.iterator();
-        
-        System.out.println("Lista de Instalaciones y Edificios");
-        
-        while (it.hasNext()) { 
-            Object item = it.next();
-            System.out.println(item.toString());
-        }
-        
 
-      
+        Iterator<Object> it = lista.iterator();
+
+        System.out.println("--- Lista de Instalaciones y Edificios ---");
+
+        while (it.hasNext()) {
+            interfazEdificio item = (interfazEdificio) it.next();
+
+            System.out.println(item.toString());
+
+            System.out.println("Método getSuperficieEdificio(): " + item.getSuperficieEdificio());
+        
+            if (item instanceof InterfazInstalacionDeportiva) {
+                InterfazInstalacionDeportiva instalacion = (InterfazInstalacionDeportiva) item;
+                System.out.println("Método getTipoDeInstalacion(): " + instalacion.getTipoDeInstalacion());
+            }
+
+            System.out.println("----------------------------------------");
+        }
     }
 }
